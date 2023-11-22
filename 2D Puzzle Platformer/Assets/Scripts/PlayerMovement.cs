e4c21da45416dd40e0b0421b44c595285d75af83
Aphrofluff <88689049+aphrofluff@users.noreply.github.com>
2023-11-22 03:38:03 -0600

    added BetterDash, BetterCameraMovement

--- 2D Puzzle Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Puzzle Platformer/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,6 @@
+using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -11,9 +14,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private PlayerWallJump _playerWallJumpScript;
+    [SerializeField] private TrailRenderer tr;
 
-    void Update()
+	private bool canDash = true;
+	private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.15f;
+	[SerializeField] private float dashingcooldown = 1f;
+
+	void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
@@ -26,6 +41,11 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
         if(!_playerWallJumpScript.isWallJumping)
         {
             Flip();
@@ -34,7 +54,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(!_playerWallJumpScript.isWallJumping)
+		if (isDashing)
+		{
+			return;
+		}
+
+		if (!_playerWallJumpScript.isWallJumping)
         {
             rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         }
@@ -54,5 +79,21 @@ public class PlayerMovement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingcooldown);
+        canDash = true;
     }
 }
